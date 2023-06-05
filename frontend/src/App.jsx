42ce3b33f59bfba507915e8f148ecf76e63015f0
kenneth-alt <kenneth.ugo15@gmail.com>
2023-06-05 11:06:15 -0600

    initialiaze react frontend with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Hello, Vite + React!</h1>
+      <p>Welcome to your new React project created with Vite.</p>
+    </div>
+  );
+}
+
+export default App
