384383384f7a82a250a6662a1b13023ba3f60db2
kenneth-alt <kenneth.ugo15@gmail.com>
2023-06-12 13:52:15 -0600

    update express server

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,38 @@
-import express from 'express';
+import express from "express";
+import { OpenAI } from "langchain/llms/openai";
+import { SqlDatabase } from "langchain/sql_db";
+import { createSqlAgent, SqlToolkit } from "langchain/agents";
+import { DataSource } from "typeorm";
 
 const app = express();
-const PORT = 5600
+const port = 5600;
 
-app.get('/', (req, res) => {
-    res.send("Hello from server")
+app.get("/api/query", async (req, res) => {
+  const datasource = new DataSource({
+    type: "mysql",
+    host: "your_host",
+    port: 8600,
+    username: "your_username",
+    password: "your_password",
+    database: "your_database",
+  });
+
+  const db = await SqlDatabase.fromDataSourceParams({
+    appDataSource: datasource,
+  });
+
+  const model = new OpenAI({ temperature: 0 });
+  const toolkit = new SqlToolkit(db, model);
+  const executor = createSqlAgent(model, toolkit);
+
+  const input = req.query.input as string;
+  const result = await executor.call({ input });
+
+  await datasource.destroy();
+
+  res.json(result.output);
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
