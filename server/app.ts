cbfaa8503b3c596c2e038dc79b55286106bf7ac3
kenneth-alt <kenneth.ugo15@gmail.com>
2023-06-17 16:37:17 -0600

    minor change

--- server/app.ts
+++ server/app.ts
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('Hello from Express server')
 })
 
