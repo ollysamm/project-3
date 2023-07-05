a0c286a0dba23be3363140e52b46d54d7257dfdb
ollysamm <125161846+ollysamm@users.noreply.github.com>
2023-07-05 13:21:53 -0600

    Demoed with the Holmetrics team. First draft.

--- support-page/src/pages/index.tsx
+++ support-page/src/pages/index.tsx
@@ -12,8 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">{siteConfig.tagline}</h1>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
