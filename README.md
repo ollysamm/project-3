d5ec2d0e0a0f19c67d33789a9c54fa445c474853
star-works <farukhkhan1115@gmail.com>
2023-07-08 12:40:54 +0530

    first commit

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ main = stable (will be final deployment)
 
 develop = main working branch, create feature branches from this and Pull request to develop
 
-Pull requests to be made to develop from features and then from develop to main (squash and merge from develop to main)
+Pull requests to be made to develop from features and then from develop to main (squash and merge from develop to main)# wendyai
--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Sidebar from "./Sidebar";
 
 interface ResponseData {
   output: string;
@@ -23,33 +24,36 @@ export const Chat = () => {
   };
 
   return (
-    <div
-      className="flex flex-col items-center justify-center"
-      style={{ padding: "20px" }}
-    >
-      <header>
-        <h1>wendyai</h1>
-      </header>
+    <>
+      <Sidebar />
+      {/* <div
+        className="flex flex-col items-center justify-center"
+        style={{ padding: "20px" }}
+      >
+        <header>
+          <h1>wendyai</h1>
+        </header>
 
-      <section>
+        <section>
+          <div>
+            <p>Text</p>
+            <h4>Wendy v1 Updated 09/06/2023</h4>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Ask Wendy..."
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </section>
+
         <div>
-          <p>Text</p>
-          <h4>Wendy v1 Updated 09/06/2023</h4>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Ask Wendy..."
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            />
-            <button type="submit">Submit</button>
-          </form>
+          <h5>{response}</h5>
         </div>
-      </section>
-
-      <div>
-        <h5>{response}</h5>
-      </div>
-    </div>
+      </div> */}
+    </>
   );
 };
