39b1f4b8d827c1ab18fa64b07c01307a3831a55d
kenneth-alt <kenneth.ugo15@gmail.com>
2023-07-12 21:58:17 -0600

    add Dockerfile

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { Icon } from '@iconify/react';
 
+interface ChatMessage {
+  userMessage: string;
+  response: string;
+}
 
 function Chat() {
-  const [inputValue, setInputValue] = useState<string>('');
-  const [response, setResponse] = useState<string>('');
+  const [userMessage, setUserMessage] = useState('');
+  const [chats, setChats] = useState<ChatMessage[]>([]);
+  const [isThinking, setIsThinking] = useState(false);
+  const [response, setResponse] = useState<AxiosResponse<any, any> | null>(null);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      const { data } = await axios.post('http://localhost:8000/ask-wendy', { prompt: inputValue });
-      setResponse(data);
+      const response = await axios.post('http://localhost:8000/ask-wendy', { prompt: userMessage });
+      setResponse(response);
+
+      const newChatMessage: ChatMessage = {
+        userMessage,
+        response: response.data,
+      };
+
+      setChats([...chats, newChatMessage]);
+      setUserMessage(''); // Clear the input field after submission
     } catch (error) {
       console.log(error);
     }
@@ -20,26 +34,29 @@ function Chat() {
 
   return (
     <div className="flex flex-col h-full justify-between m-4 border border-black">
-      
       <div>
         <h4>Wendy v1 Updated 09/06/2023</h4>
       </div>
 
       <div>
         <div>
-          <h5>{response}</h5>
+          {chats.map((chat, index) => (
+            <div key={index}>
+              <h5>{chat.response}</h5>
+              <h5>{chat.userMessage}</h5>
+            </div>
+          ))}
         </div>
 
         <div>
           <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Ask Wendy..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            <input type="text" placeholder="Ask Wendy..." value={userMessage} onChange={(e) => setUserMessage(e.target.value)} />
             <button type="submit"><Icon icon="majesticons:send" /></button>
           </form>
         </div>
       </div>
-      
     </div>
   );
-};
+}
 
 export default Chat;
--- server/main.py
+++ server/main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
