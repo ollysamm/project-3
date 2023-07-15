9b20e553b399c45c1ba51d81894dfcafb7c29c3c
kenneth-alt <kenneth.ugo15@gmail.com>
2023-07-15 06:45:56 -0600

    add responsive hamburger menu to Sidebar

--- client/src/layouts/sidebar/index.tsx
+++ client/src/layouts/sidebar/index.tsx
@@ -1,30 +1,73 @@
-import React from "react";
-import { motion } from "framer-motion"
-
-import { Logo } from "../../components/logo"
+import React, { useState, useEffect } from "react";
+import { BiMenu } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
+import { Logo } from "../../components/logo";
 import { LoggedInUser } from "../../components/loggedInUser";
-import { NewChat } from "../../components/newChat";
 import { ChatHistory } from "../../components/chatHistory";
 import { Separator } from "../../components/separator";
-import { SideFooter } from "../../components/sideFooter"
+import { SideFooter } from "../../components/sideFooter";
 
 function Sidebar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newWindowWidth = window.innerWidth;
+      setWindowWidth(newWindowWidth);
+
+      if (newWindowWidth < 768) {
+        setShowSidebar(false);
+      } else {
+        setShowSidebar(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Check initial window size
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const closeSidebar = () => {
+    setShowSidebar(false);
+  };
+
   return (
     <div>
-      <motion.div className="flex flex-col justify-between bg-hol-blue-dark text-white shadow-xl z-[999] w-64 max-w-64 p-3 h-screen overflow-hidden md:relative fixed">
-        <div>
-          <Logo />
-          <LoggedInUser />
-          <ChatHistory />
+      {showSidebar ? (
+        <div className="flex flex-col justify-between bg-hol-blue-dark text-white shadow-xl z-[999] w-64 max-w-64 p-3 pt-4 h-screen overflow-auto md:relative fixed">
+          <div>
+            {showSidebar && windowWidth < 768 && (
+              <div className="flex justify-end mb-3">
+                <button className="close-icon" onClick={closeSidebar}>
+                  <IoClose size={24} />
+                </button>
+              </div>
+            )}
+            <Logo />
+            <LoggedInUser />
+            <ChatHistory />
+          </div>
+
+          <div>
+            <Separator />
+            <SideFooter />
+          </div>
         </div>
-        
-        <div>
-          <Separator />
-          <SideFooter />
-        </div>
-      </motion.div>
+      ) : (
+        <button className="hamburger-icon p-2" onClick={toggleSidebar}>
+          <BiMenu size={24} />
+        </button>
+      )}
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
