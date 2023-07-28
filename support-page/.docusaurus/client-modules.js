8fbda567f56e4c4c640ddf0ccedb37bf2d23466f
ollysamm <125161846+ollysamm@users.noreply.github.com>
2023-07-28 10:21:27 -0600

    Docusaurus 2nd build

--- support-page/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\olive\\OneDrive\\Documents\\InceptionU\\Projects\\Project 3\\project-3\\support-page\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\olive\\OneDrive\\Documents\\InceptionU\\Projects\\Project 3\\project-3\\support-page\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\olive\\OneDrive\\Documents\\InceptionU\\Projects\\Project 3\\project-3\\support-page\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\olive\\OneDrive\\Documents\\InceptionU\\Projects\\Project 3\\project-3\\support-page\\src\\css\\custom.css'),
-];
